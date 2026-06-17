00cbd139df2f7b3b574dbfb6d20076e4761bbae6
Yassin-2 <kanajahtresor@gmail.com>
2026-06-17 18:18:48 +0200

    'saved'

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -12,20 +12,6 @@ import CancellationPolicy from "../pages/CancellationPolicy/index";
 import CookiePolicy from "../pages/CookiePolicy/index";
 import NotFound from "../pages/NotFound/index";
 
-import DashboardLayoutFallback from "../dashboard/DashboardLayoutFallback";
-
-import DashboardPage from "../dashboard/Dashboard/DashboardPage";
-
-import RoomsManager from "../dashboard/Dashboard/Rooms/RoomsManager";
-import ExperiencesManager from "../dashboard/Dashboard/Experiences/ExperiencesManager";
-import PagesList from "../dashboard/Dashboard/Pages/PagesList";
-import PageEditor from "../dashboard/Dashboard/Pages/PageEditor";
-import SiteSettings from "../dashboard/Dashboard/Settings/SiteSettings";
-
-import LoginPage from "../dashboard/Login/LoginPage";
-
-import ProtectedRoute from "../components/ProtectedRoute";
-
 export default function AppRoutes() {
   return (
     <Routes>
@@ -39,25 +25,6 @@ export default function AppRoutes() {
         <Route path="terms-conditions" element={<TermsConditions />} />
         <Route path="cancellation-policy" element={<CancellationPolicy />} />
         <Route path="cookie-policy" element={<CookiePolicy />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-
-      <Route path="/login" element={<LoginPage />} />
-
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <DashboardLayoutFallback />
-          </ProtectedRoute>
-        }
-      >
-        <Route index element={<DashboardPage />} />
-        <Route path="rooms" element={<RoomsManager />} />
-        <Route path="experiences" element={<ExperiencesManager />} />
-        <Route path="pages" element={<PagesList />} />
-        <Route path="pages/:id/edit" element={<PageEditor />} />
-        <Route path="settings" element={<SiteSettings />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
